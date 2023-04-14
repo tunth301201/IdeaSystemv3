d9602fa39b7cf1eee70c9e35e019b516c4a6f402
Selina <tunth301201@gmail.com>
2023-04-14 22:20:44 +0700

    delete ideauser

--- routes/userIdea.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userIdeaController = require('../controllers/userIdea.controller');
-
-// PUT /userIdeas
-router.put('/:id', userIdeaController.updateRating);
-
-module.exports = router;
